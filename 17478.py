f593a61f22b6d13c540a3a3df88cd9d9010c047b
ChaeyoonWoo <heeni1004@gmail.com>
2024-07-17 16:40:40 +0900

    17478 재귀 구현

--- 17478.py
+++ 17478.py
@@ -8,13 +8,14 @@ def slash(n):
 N = int(input())
 
 print("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.")
+
 q = '\"재귀함수가 뭔가요?"'
 a1 = '\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.'
 a2 = "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지."
 a3 = '그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"'
 
 def recursion(n):
-    if (N==n):
+    if (N == n):
         slash(n)
         print(q)
         slash(n)
@@ -31,11 +32,11 @@ def recursion(n):
     print(a3)
 
     recursion(n+1)
-    
-    slash(n)
+
     print("라고 답변하였지.")
+    slash(n)
 
 recursion(0)
-
+print("라고 답변하였지.")
 
 
