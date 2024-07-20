597bc920c0bfdb020769b613e363ebd36cde61a3
ChaeyoonWoo <heeni1004@gmail.com>
2024-07-20 12:18:24 +0900

    feat: 2630풀이 추가

--- 2630.py
+++ 2630.py
@@ -12,7 +12,29 @@ for i in range(N):
 blue = 0
 white = 0
 
-def cut(x,y,n):
+def cut(x,y,N):
   global blue
   global white
+
+  k = arr[x][y]
+
+  for i in range(x, x+N):
+    for j in range(y, y+N):
+      if k != arr[i][j]:
+        cut(x, y, N//2)
+        cut(x, y+N//2, N//2)
+        cut(x+N//2, y, N//2)
+        cut(x+N//2, y+N//2, N//2)
+        return
+    
+  if k == 0:
+    white += 1
+  else: 
+    blue += 1
+    
+cut(0, 0, N)
+
+print(white)
+print(blue)
+
 
